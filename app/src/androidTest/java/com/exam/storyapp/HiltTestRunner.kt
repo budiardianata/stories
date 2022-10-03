a550a84ada076e57e7816a769f7721f339b83e10
budiardianata <budiardianata@users.noreply.github.com>
2022-10-03 12:56:07 +0700

    feat: setup HiltTestRunner

---
+++ app/src/androidTest/java/com/exam/storyapp/HiltTestRunner.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2022 Budi Ardianata.
+ */
+package com.exam.storyapp
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HiltTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
---
+++ app/src/main/java/com/exam/storyapp/common/util/HiltTestActivity.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2022 Budi Ardianata.
+ */
+package com.exam.storyapp.common.util
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
